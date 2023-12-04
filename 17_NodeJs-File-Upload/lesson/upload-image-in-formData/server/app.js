744e4275e735f9ef354e38a94591183fead7b442
nrcool <nomanalam7214@gmail.com>
2023-12-04 11:20:57 +0100

    upload image to backend

---
+++ 17_NodeJs-File-Upload/lesson/upload-image-in-formData/server/app.js
@@ -0,0 +1,38 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import fileUpload from "express-fileupload"
+import ImageModel from "./models/imageSchema.js"
+
+try{
+    await mongoose.connect("mongodb://127.0.0.1:27017/fileupload-db")
+    console.log("DB connection established !")
+}
+catch(err){
+    console.log(err)
+}
+
+const app = express()
+const PORT = 5000;
+
+app.use(cors({origin:"http://localhost:5173"}))
+app.use(express.json({limit:"100mb"})) //default limit is 2MB 
+app.use(fileUpload(/* {useTempFiles:true,  tempFileDir:"./upload"} */))
+
+
+//create POST api/images endpoint
+
+app.post("/api/images",async (req,res)=>{
+    console.log(req.body)
+
+    console.log(req.files)
+    const image = await ImageModel.create({
+        filename:Date.now() +"_" +req.files.file.name , 
+        data : req.files.file.data
+    })
+
+    res.send({okay:image})
+})
+
+
+app.listen(PORT, ()=>console.log("server is running on port: ",PORT))
---
+++ 17_NodeJs-File-Upload/lesson/upload-image-in-formData/server/models/imageSchema.js
@@ -0,0 +1,13 @@
+import {Schema,model} from "mongoose"
+
+
+const ImageSchema = new Schema({
+filename: {type:String, required:true},
+data: {type:Buffer}
+})
+
+
+const ImageModel = model("images",ImageSchema)
+
+
+export default ImageModel;
